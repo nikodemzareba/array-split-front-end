1864624b2b387ea46986fc25ee89ea4068a1319c
Nikodem Zareba <nikodem.zareba00@gmail.com>
2023-07-21 15:25:36 +0100

    algo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // Function to divide array into N groups
+  function groupArrayElements(arr, N) {
+    const size = Math.floor(arr.length / N);
+    const remainder = arr.length % N;
+
+    let groups = [];
+    let start = 0;
+    for (let i = 0; i < N; i++) {
+      let end = (i < remainder) ? start + size + 1 : start + size;
+      groups.push(arr.slice(start, end));
+      start = end;
+    }
+    
+    return groups;
+  }
+
+  const [result, setResult] = useState(null);
+
+  if (!result) {
+    setResult(groupArrayElements([1, 2, 3, 4, 5], 3));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <h1 className="Header">Result:</h1>
+        <p className="Output">{ JSON.stringify(result) }</p>
+      </div>
     </div>
   );
 }
